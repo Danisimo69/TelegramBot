e5771765202c80569c5d1b86d5b2705718703968
ikasaramikas <nsi4b@yandex.com>
2023-12-04 17:44:18 +0300

    Update v0.11

--- Filters/UserFilters.py
+++ Filters/UserFilters.py
@@ -4,17 +4,29 @@ from aiogram.types import Message, CallbackQuery
 
 class Has_One_Chan_Filter(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
-        return True if callback.data.split("_")[0] == "one" or callback.data.split("_")[0] == "chan" else False
+        try:
+            return True if callback.data.split("_")[0] == "one" or callback.data.split("_")[0] == "chan" else False
+        except:
+            return False
 
 class Get_Buy_Message_Filter(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
-        return True if callback.data.isdigit() and int(callback.data) < 7 else False
+        try:
+            return True if callback.data.isdigit() and int(callback.data) < 7 else False
+        except:
+            return False
 
 class Redact_Card_Filter(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
-        return True if callback.data.split("_")[1] in ["old", "new"] else False
+        try:
+            return True if callback.data.split("_")[1] in ["old", "new"] else False
+        except:
+            return False
 
 class Show_All_Cards_Filter(BaseFilter):
     async def __call__(self, callback: CallbackQuery) -> bool:
-        return True if "choose_" in callback.data or "redact_" in callback.data or "destroy_" in callback.data else False
+        try:
+            return True if "choose_" in callback.data or "redact_" in callback.data or "destroy_" in callback.data else False
+        except:
+            return False
 
